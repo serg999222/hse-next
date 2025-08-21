a17725d3cbdce98278bd9a0e417fc5f99dcbc37e
Hatsenko Serhii <gacenkosergij160@gmail.com>
2025-08-21 17:54:35 +0300

    disable eslint rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ⚡ При build ESLint не буде блокувати збірку
+    ignoreDuringBuilds: true,
+  },
+  /* інші config options тут */
 };
 
 export default nextConfig;
+
